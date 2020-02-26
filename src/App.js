68538ff1f47c29bb30b254b9efdfb7a226fee6ef
Frederick Johnson <fritzj96@gmail.com>
2020-02-26 10:22:43 -0800

    added: linting

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Router>
       <Layout>
-
+        
       </Layout>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,15 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
 const CustomNavbar = () => (
-  <Navbar bg="primary" variant='dark' expand="lg">
+  <Navbar bg="primary" variant="dark" expand="lg">
     <LinkContainer exact to="/">
-    <Navbar.Brand>Water Exchange</Navbar.Brand>
+      <Navbar.Brand>The Water Agency Inc.</Navbar.Brand>
     </LinkContainer>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <LinkContainer exact to="/">
-        <Nav.Link>Listings</Nav.Link>
+        <LinkContainer exact to="/listings">
+          <Nav.Link>Listings</Nav.Link>
         </LinkContainer>
         <LinkContainer exact to="/offers">
           <Nav.Link>Offers</Nav.Link>
