367f5ea0f6e0cd76d3332ea8cf1bdf64d8cf34e9
Frederick Johnson <fritzj96@gmail.com>
2020-02-27 10:26:35 -0800

    added: AuthUser context to app & navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { useState, useEffect, useContext } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import { Layout } from "./components/Layout";
 import { LoginPage } from "./pages/LoginPage";
+import { FirebaseContext } from "./firebase/firebase";
+import { AuthUserContext } from "./firebase/authUser";
 
 function App() {
+  const [authUser, setAuthUser] = useState(null);
+  const Firebase = useContext(FirebaseContext);
+  useEffect(() => {
+    const listener = Firebase.auth.onAuthStateChanged(user => {
+      user ? setAuthUser(user) : setAuthUser(null);
+    });
+    return () => listener();
+  }, [Firebase.auth]);
   return (
-    <Router>
-      <Layout>
-        <Route path="/login" component={LoginPage} />
-      </Layout>
-    </Router>
+    <AuthUserContext.Provider value={authUser}>
+      <Router>
+        <Layout>
+          <Route path="/login" component={LoginPage} />
+        </Layout>
+      </Router>
+    </AuthUserContext.Provider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import { AuthUserContext } from "../firebase/authUser";
 
-const CustomNavbar = () => (
-  <Navbar bg="primary" variant="dark" expand="lg">
-    <Navbar.Brand className="mb-0" as="h2">The Water Agency Inc.</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="mr-auto">
-        <LinkContainer exact to="/listings">
-          <Nav.Link>Listings</Nav.Link>
-        </LinkContainer>
-        <LinkContainer exact to="/offers">
-          <Nav.Link>Offers</Nav.Link>
-        </LinkContainer>
-        <LinkContainer exact to="/profile">
-          <Nav.Link>Profile</Nav.Link>
-        </LinkContainer>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-);
-
+const CustomNavbar = () => {
+  const authUser = useContext(AuthUserContext);
+  return (
+    <Navbar bg="primary" variant="dark" expand="lg">
+      <Navbar.Brand className="mb-0" as="h2">
+        The Water Agency Inc.
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        {authUser && (
+          <Nav className="mr-auto">
+            <LinkContainer exact to="/listings">
+              <Nav.Link>Listings</Nav.Link>
+            </LinkContainer>
+            <LinkContainer exact to="/offers">
+              <Nav.Link>Offers</Nav.Link>
+            </LinkContainer>
+            <LinkContainer exact to="/profile">
+              <Nav.Link>Profile</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        )}
+        {!authUser && (
+          <Nav className="mr-auto">
+            <LinkContainer exact to="/login">
+              <Nav.Link>Login</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          )}
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 export default CustomNavbar;
