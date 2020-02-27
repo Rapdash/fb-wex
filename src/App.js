550dd1baf40963c4920a21e7ba26f8ff525c3dc0
Frederick Johnson <fritzj96@gmail.com>
2020-02-27 10:39:48 -0800

    addded: ResetPasswordPage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Layout } from "./components/Layout";
 import { LoginPage } from "./pages/LoginPage";
 import { FirebaseContext } from "./firebase/firebase";
 import { AuthUserContext } from "./firebase/authUser";
+import { ResetPasswordPage } from "./pages/ResetPasswordPage";
 
 function App() {
   const [authUser, setAuthUser] = useState(null);
@@ -16,10 +17,11 @@ function App() {
     return () => listener();
   }, [Firebase.auth]);
   return (
-    <AuthUserContext.Provider value={authUser}>
+     <AuthUserContext.Provider value={authUser}>
       <Router>
         <Layout>
           <Route path="/login" component={LoginPage} />
+          <Route path="/reset-password" component={ResetPasswordPage} />
         </Layout>
       </Router>
     </AuthUserContext.Provider>
---
+++ src/pages/ResetPasswordPage.js
@@ -0,0 +1,40 @@
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Form, Button } from "react-bootstrap";
+
+import { FirebaseContext } from "../firebase/firebase";
+
+export const ResetPasswordPage = () => {
+  const Firebase = useContext(FirebaseContext)
+  const [email, setEmail] = useState('');
+
+  const onSubmit = e => {
+    e.preventDefault();
+    Firebase.resetPassword(email);
+  };
+  return (
+    <Row className="mt-5">
+      <Col md={{ span: 6, offset: 3 }} lg={{ span: 4, offset: 4 }}>
+        <Card>
+          <Card.Header>
+            <Card.Title className="text-center display-4">Reset Password</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Form onSubmit={e => onSubmit(e)}>
+              <Form.Group controlId="formEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control required type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)} />
+              </Form.Group>
+              <Button type="submit" size="lg" block>Send Password Reset Email</Button>
+            </Form>
+          </Card.Body>
+          <Card.Footer className="text-right">
+              <Link to="/forgot-password">
+                Forgot your password? Click Here To Reset It.
+              </Link>
+          </Card.Footer>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
