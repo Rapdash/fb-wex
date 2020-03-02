5d86ec4815cd4501ac4da1a7c691b74e23d3f776
Frederick Johnson <fritzj96@gmail.com>
2020-03-02 11:54:35 -0800

    removed: old auth scheme

--- src/util/checkAuth.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
-
-export const checkAuth = (authUser) => {
-  if (!authUser) return <Redirect to="/login"/>;
-  return null;
-}
